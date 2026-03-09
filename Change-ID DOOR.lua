d6f0076bc851515ccc7fb11d050a11a5ae0e750f
FattyZing <minecraftyazing@gmail.com>
2026-03-09 20:43:26 +0800

    Update and rename

---
+++ Change-ID DOOR.lua
@@ -0,0 +1,4 @@
+X, Y = 60, 24-- Koordinat Door
+SendPacket(2, [[action|dialog_return dialog_name|door_edit tilex|]]..X..[[|tiley|]]..Y..[[|door_name|door_target|door_id|Dayycheckbox_locked|1]])
+log("`2Change DoorID to : `cDayy")
+log("`bThe newest door id is : `2Dayy")
